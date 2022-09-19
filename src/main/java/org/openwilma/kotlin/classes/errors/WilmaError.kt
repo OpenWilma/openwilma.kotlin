95a8e88d54dbac7ed761a927448939141a6be624
Developer From Jokela <developerfromjokela@gmail.com>
2022-09-19 11:17:34 +0300

    Added message to WilmaError object

--- src/main/java/org/openwilma/kotlin/classes/errors/WilmaError.kt
+++ src/main/java/org/openwilma/kotlin/classes/errors/WilmaError.kt
@@ -1,9 +1,10 @@
 package org.openwilma.kotlin.classes.errors
 
 import com.google.gson.annotations.SerializedName
+import java.util.StringJoiner
 
 class WilmaError(
-    message: String?, @field:SerializedName("id") var errorID: String, @field:SerializedName(
+    @field:SerializedName("message") override var message: String?, @field:SerializedName("id") var errorID: String, @field:SerializedName(
         "description"
     ) var description: String, @field:SerializedName("whatnext") var whatsnext: String
 ) : Error(message, ErrorType.WilmaError) {
